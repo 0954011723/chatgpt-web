f2585aad6886e2272ca657eafac7150959a45ff3
lover <39675911+0954011723@users.noreply.github.com>
2023-04-14 05:52:04 +0800

    解決 vscode 會出現找不到  App.vue 模組問題

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,5 @@
-declare module '*.vue' {
+// 解決 vscode 會出現找不到  App.vue 模組問題
+declare module '*.vue' {	
   import { ComponentOptions } from 'vue'
   const componentOptions: ComponentOptions
   export default componentOptions
